13e25df394be6b0b64652bd086384391624ba073
Haizea Martin <hmartinalz@uoc.edu>
2022-04-07 21:55:28 +0200

    build project

---
+++ dist/detalle.177c8253.js
@@ -0,0 +1,2 @@
+!function(){var e=moment().format("YYYY/MM/DD");document.getElementById("date").innerHTML=e}();
+//# sourceMappingURL=detalle.177c8253.js.map
---
+++ dist/detalle.a1b7c6db.js
@@ -0,0 +1,2 @@
+let e=moment().format("YYYY/MM/DD");document.getElementById("date").innerHTML=e;
+//# sourceMappingURL=detalle.a1b7c6db.js.map
